1a1301c8d8eda3c2a2c462429af5b9296f332ceb
GitHub Action <action@github.com>
2021-04-28 08:22:32 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/wangsongbin001/ideplugintemplate/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.wangsongbin001.ideplugintemplate.services
+
+import com.github.wangsongbin001.ideplugintemplate.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/wangsongbin001/ideplugintemplate/services/MyProjectService.kt
+++ src/main/kotlin/com/github/wangsongbin001/ideplugintemplate/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.wangsongbin001.ideplugintemplate.services
 
+import com.github.wangsongbin001.ideplugintemplate.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
